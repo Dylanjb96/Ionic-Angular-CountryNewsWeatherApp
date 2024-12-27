5c3191996493b6f7a41117364a028b9065a9b037
Dylan Boyle <dylan96.boyle@gmail.com>
2024-12-27 04:46:19 +0000

    Added recent search country feature on home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,7 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonLabel,
+  IonList,
+  IonListHeader,
+  IonTitle, IonToolbar,
+} from '@ionic/angular/standalone';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   standalone: true,
@@ -19,13 +25,16 @@ import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonInput, IonIte
     IonIcon, 
     IonButtons, 
     IonItem, 
-    IonLabel, 
+    IonLabel,
+    IonList,
+    IonListHeader, 
     IonInput],
 })
 export class HomePage {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private storage: Storage) {}
 
   countryName: string = ''; //Holds the entered country name
+  recentSearches: string[] = []; 
 
   //Navigate to the Settings page
   navigateToSetting() {
@@ -34,12 +43,39 @@ export class HomePage {
   }
   
   //Navigate to the Countries Page eith the entered country name
-  getCountries() {
+ async getCountries() {
     if (this.countryName.trim() && /^[a-zA-Z\s]+$/.test(this.countryName)) {
       console.log('Navigating to countries page with:', this.countryName);
+      //Save the search and navigate
+      await this.saveToRecentSearch(this.countryName);
       this.router.navigate(['/countries'], { queryParams: { name: this.countryName } });
     } else {
       alert('Please enter a valid country name');
     }
+  }
+
+  async ngOnInit() {
+    //Start Ionic storage and load recent searches
+    await this.storage.create();
+    this.loadRecentSearch();
+  }
+
+  async loadRecentSearch() {
+    //Load recent search from storage
+    this.recentSearches = (await this.storage.get('recentSearches')) || [];
+  }
+
+  async saveToRecentSearch(country: string) {
+    //Add a country to recent searches if not already present
+    if (!this.recentSearches.includes(country)) {
+      this.recentSearches.unshift(country); // Add to the start of the array
+      this.recentSearches = this.recentSearches.slice(0, 5); // Limit to 5 items
+      await this.storage.set('recentSearches', this.recentSearches); // Save to storage
+    }
+  }
+
+  async selectRecentSearch(country: string) {
+    this.countryName = country;
+    await this.getCountries();
   }
 }  
